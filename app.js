0cb7483892bfe7ddbba5c9986062b4b4bb925729
sambndhmmatrimony <sambndhmmatrimony@gmail.com>
2026-09-17 21:02:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("SAHNAI SERVICE POINT MATRIMONY loaded successfully.");
+});
+
+function showMessage(message) {
+  alert(message);
+}
